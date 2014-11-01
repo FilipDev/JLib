bebe8b03aefb822c089c947c016ae0b8752a59f7
Administrator <thespherret@gmail.com>
2014-11-01 19:31:03 -0400

    Main class now cleaned main method.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import me.pauzen.jlib.mbean.MBeanServerWrapper;
-import me.pauzen.jlib.mbean.hotspot.HotSpotDiagnostic;
-
 import javax.management.*;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -8,13 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
 
     public static void main(String[] args) throws MalformedObjectNameException, MBeanException, InstanceNotFoundException, ReflectionException, IntrospectionException, AttributeNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstanceAlreadyExistsException, IOException, NotCompliantMBeanException, ClassNotFoundException {
-
-        MBeanServerWrapper.testMethod();
-
-
-
-        System.out.println(HotSpotDiagnostic.getInstance().getAlignment());
-
+        //USED FOR TESTING
     }
 
 }
