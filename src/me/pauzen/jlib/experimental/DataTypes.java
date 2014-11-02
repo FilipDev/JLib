3b7feb4ff2e24c9c170dc069410e20e29ce10206
Administrator <thespherret@gmail.com>
2014-11-02 13:42:34 -0500

    Experimental DataTypes class added, contains converter from byte array to int.

---
+++ src/me/pauzen/jlib/experimental/DataTypes.java
@@ -0,0 +1,20 @@
+package me.pauzen.jlib.experimental;
+
+import java.nio.ByteBuffer;
+
+public final class DataTypes {
+
+    private DataTypes() {
+    }
+
+    /**
+     * Converts a byte array to an int. An int is 4 bytes.
+     *
+     * @param bytes Byte array to convert to int.
+     * @return Int value of bytes.
+     */
+    public static int toInt(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+}
