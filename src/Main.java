2847968a937672bfb422999d94a36583c668e8bb
Administrator <thespherret@gmail.com>
2014-12-19 23:21:24 -0500

    Reworked Http system. Added BoolLogic class. Added StringWriter and StringReader classes.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
-import javax.management.*;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import me.pauzen.jlib.http.Http;
+import me.pauzen.jlib.http.headers.UserAgent;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedObjectNameException, MBeanException, InstanceNotFoundException, ReflectionException, IntrospectionException, AttributeNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstanceAlreadyExistsException, IOException, NotCompliantMBeanException, ClassNotFoundException {
-        //USED FOR TESTING FEATURES
+    public Main() throws IllegalAccessException {
+        System.out.println(Http.post("https://mostwantedhf.info/").header(UserAgent.CHROME_WINDOWS).field("skypeName", "heycallmethemezand").field("resolve", "Grab IP").getResult().getFile().toString());
     }
+
+    public double compund(double initial, double rate, int times) {
+        if (times == 0) return 1;
+        if (times == 1) return initial;
+        return compund(initial * (1 + rate), rate, times - 1);
+    }
+
+    private int times = 0;
+    public double getTimesDoubled(double n) {
+        times = 0;
+        return timesDoubled(n);
+    }
+    public double timesDoubled(double n) {
+        if (n <= 1) return times;
+        times++;
+        System.out.println(n);
+        return timesDoubled(Math.sqrt(n));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+    }
+
+    public class Test<T> {
+    }
+
 }
